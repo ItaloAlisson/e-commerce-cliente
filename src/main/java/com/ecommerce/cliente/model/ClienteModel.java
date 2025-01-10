1ed10ad008fc76f5bd52cdbf2f46ec8ded0c8c0d
Italo Alisson <italoalisson15@gmail.com>
2025-01-09 23:22:34 -0300

    :sparkles: feat: adiciona model cliente e configurações do banco

---
+++ src/main/java/com/ecommerce/cliente/model/ClienteModel.java
@@ -0,0 +1,43 @@
+package com.ecommerce.cliente.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity()
+@Table(name = "CLIENTES")
+@Getter
+@Setter
+public class ClienteModel implements Serializable {
+
+    private static final long serialversionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String dataNascimento;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false, unique = true)
+    private String cpf;
+    @Column(nullable = false, columnDefinition = "INT DEFAULT 1")
+    private Integer status;
+
+    public ClienteModel() {
+    }
+
+    public ClienteModel(UUID id, String nome, String dataNascimento, String email, String cpf, Integer status) {
+        this.id = id;
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+        this.email = email;
+        this.cpf = cpf;
+        this.status = status;
+    }
+}
+
