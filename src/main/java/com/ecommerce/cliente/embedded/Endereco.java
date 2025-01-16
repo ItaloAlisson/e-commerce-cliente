2ec60bd543fad3eba6fcfb8359cf4b8b0f2425d7
Italo Alisson <italoalisson15@gmail.com>
2025-01-16 20:04:17 -0300

    :sparkles: feat: adiciona validações aos atributos da classe Endereco

--- src/main/java/com/ecommerce/cliente/embedded/Endereco.java
+++ src/main/java/com/ecommerce/cliente/embedded/Endereco.java
@@ -2,6 +2,7 @@ package com.ecommerce.cliente.embedded;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,16 +10,22 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Endereco {
+    @NotBlank(message ="Campo 'logradouro' não pode estar vazio!")
     @Column(nullable = false)
     private String logradouro;
+    @NotBlank(message ="Campo 'numero' não pode estar vazio!")
     @Column(nullable = false)
     private String numero;
+    @NotBlank(message ="Campo 'bairro' não pode estar vazio!")
     @Column(nullable = false)
     private String bairro;
+    @NotBlank(message ="Campo 'cidade' não pode estar vazio!")
     @Column(nullable = false)
     private String cidade;
+    @NotBlank(message ="Campo 'estado' não pode estar vazio!")
     @Column(nullable = false)
     private String estado;
+    @NotBlank(message ="Campo 'cep' não pode estar vazio!")
     @Column(nullable = false)
     private String cep;
 
