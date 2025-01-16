a001fe875c75a08dfa4f43b685eec30a400e31be
Italo Alisson <italoalisson15@gmail.com>
2025-01-16 20:49:42 -0300

    :sparkles: feat: adiciona estrutura inicial do controller

---
+++ src/main/java/com/ecommerce/cliente/controllers/ClienteController.java
@@ -0,0 +1,13 @@
+package com.ecommerce.cliente.controllers;
+
+import com.ecommerce.cliente.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/cliente")
+public class ClienteController {
+    @Autowired
+    ClienteService clienteService;
+}
