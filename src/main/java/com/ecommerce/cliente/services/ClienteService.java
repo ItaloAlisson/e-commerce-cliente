261aed0dba0058af9629a4be7b666ae82432cce6
Italo Alisson <italoalisson15@gmail.com>
2025-01-16 20:54:43 -0300

    :sparkles: feat: adiciona estrutura inicial do service

---
+++ src/main/java/com/ecommerce/cliente/services/ClienteService.java
@@ -0,0 +1,12 @@
+package com.ecommerce.cliente.services;
+
+import com.ecommerce.cliente.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteService {
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+}
