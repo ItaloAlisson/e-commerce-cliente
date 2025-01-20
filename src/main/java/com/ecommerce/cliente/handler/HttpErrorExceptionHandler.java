6cfb9eacdc1b396df23c9ede3f2cd4569ec9fda8
Italo Alisson <italoalisson15@gmail.com>
2025-01-19 22:22:52 -0300

    :sparkles: feat: adiciona handler para determinadas exceções

---
+++ src/main/java/com/ecommerce/cliente/handler/HttpErrorExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.ecommerce.cliente.handler;
+
+import com.ecommerce.cliente.exceptions.BadRequestException;
+import com.ecommerce.cliente.exceptions.ConflictException;
+import org.hibernate.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class HttpErrorExceptionHandler {
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<String> conflict(ConflictException exception) {
+        return ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(exception.getMessage());
+    }
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<String> notFound(ObjectNotFoundException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(
+                "Não foi encontrado o " +
+                        exception.getEntityName() +
+                        " com o ID" +
+                exception.getIdentifier());
+
+    }
+
+    public ResponseEntity<String> BadRequest(BadRequestException exception){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(exception.getMessage());
+    }
+
+
+}
