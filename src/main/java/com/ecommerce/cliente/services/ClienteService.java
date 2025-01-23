d8eb882dc4428dcfaabc9c53ae0c001cb780c2a7
Italo Alisson <italoalisson15@gmail.com>
2025-01-22 22:10:15 -0300

    :sparkles: feat: adiciona adiciona método registrarCliente e injeções de dependência de ClienteValidator e ClienteMapper.

--- src/main/java/com/ecommerce/cliente/services/ClienteService.java
+++ src/main/java/com/ecommerce/cliente/services/ClienteService.java
@@ -1,12 +1,40 @@
 package com.ecommerce.cliente.services;
 
+import com.ecommerce.cliente.dtos.ClienteRecordDTO;
+import com.ecommerce.cliente.embedded.Endereco;
+import com.ecommerce.cliente.mappers.ClienteMapper;
+import com.ecommerce.cliente.models.ClienteModel;
 import com.ecommerce.cliente.repositories.ClienteRepository;
+import com.ecommerce.cliente.validation.ClienteValidator;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Service
 public class ClienteService {
 
     @Autowired
     private ClienteRepository clienteRepository;
+
+    @Autowired
+    private ClienteValidator clienteValidator;
+
+    @Autowired
+    private ClienteMapper clienteMapper;
+
+    @Transactional
+    public ResponseEntity<Object> registrarCliente(ClienteRecordDTO clienteDTO) {
+        clienteValidator.existePorCpf(clienteDTO.cpf());
+        clienteValidator.existePorEmail(clienteDTO.email());
+        ClienteModel clienteModel = clienteMapper.dtoParaModel(clienteDTO);
+        Endereco endereco = clienteMapper.enderecoDTOParaEndereco(clienteDTO.endereco());
+        clienteModel.setEndereco(endereco);
+       clienteRepository.save(clienteModel);
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body("Cliente registrado com sucesso!");
+    }
 }
