745942f234a20f58afd667592220a47718d1fedb
Italo Alisson <italoalisson15@gmail.com>
2025-01-22 22:06:07 -0300

    :sparkles: feat: adiciona método registrarCliente no ClienteController

--- src/main/java/com/ecommerce/cliente/controllers/ClienteController.java
+++ src/main/java/com/ecommerce/cliente/controllers/ClienteController.java
@@ -1,7 +1,12 @@
 package com.ecommerce.cliente.controllers;
 
+import com.ecommerce.cliente.dtos.ClienteRecordDTO;
 import com.ecommerce.cliente.services.ClienteService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ClienteController {
     @Autowired
     private ClienteService clienteService;
+
+    @PostMapping()
+    public ResponseEntity<Object> registrarCliente(@RequestBody @Valid ClienteRecordDTO clienteDTO) {
+
+        return clienteService.registrarCliente(clienteDTO);
+    }
+
+
 }
