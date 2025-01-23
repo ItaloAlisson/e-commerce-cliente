34503b7c061e9f28c81b315635f195fae06e19ab
Italo Alisson <italoalisson15@gmail.com>
2025-01-22 22:12:31 -0300

    :sparkles: feat: adiciona no ClienteRepository métodos de consulta de existência por cpf e e-mail

--- src/main/java/com/ecommerce/cliente/repositories/ClienteRepository.java
+++ src/main/java/com/ecommerce/cliente/repositories/ClienteRepository.java
@@ -1,6 +1,7 @@
 package com.ecommerce.cliente.repositories;
 
 import com.ecommerce.cliente.models.ClienteModel;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,7 @@ import java.util.UUID;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<ClienteModel, UUID> {
+    boolean existsByCpf(String cpf);
+
+    boolean existsByEmail(String email);
 }
