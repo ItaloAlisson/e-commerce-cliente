0f64b3862c245da59ed01290c5b27c347a7cf0c2
Italo Alisson <italoalisson15@gmail.com>
2025-01-22 21:52:09 -0300

    :recycle: refactor: remove validações dos atributos da classe Endereco

--- src/main/java/com/ecommerce/cliente/embedded/Endereco.java
+++ src/main/java/com/ecommerce/cliente/embedded/Endereco.java
@@ -2,7 +2,6 @@ package com.ecommerce.cliente.embedded;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,22 +9,22 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Endereco {
-    @NotBlank(message ="Campo 'logradouro' não pode estar vazio!")
+
     @Column(nullable = false)
     private String logradouro;
-    @NotBlank(message ="Campo 'numero' não pode estar vazio!")
+
     @Column(nullable = false)
     private String numero;
-    @NotBlank(message ="Campo 'bairro' não pode estar vazio!")
+
     @Column(nullable = false)
     private String bairro;
-    @NotBlank(message ="Campo 'cidade' não pode estar vazio!")
+
     @Column(nullable = false)
     private String cidade;
-    @NotBlank(message ="Campo 'estado' não pode estar vazio!")
+
     @Column(nullable = false)
     private String estado;
-    @NotBlank(message ="Campo 'cep' não pode estar vazio!")
+
     @Column(nullable = false)
     private String cep;
 
